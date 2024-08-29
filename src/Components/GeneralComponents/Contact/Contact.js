9d2ca014165cb182c3465daea380cce9d116e5d4
oo11boy <editkhone@gmail.com>
2024-08-29 22:58:56 +0430

    feat:Expand the top header

---
+++ src/Components/GeneralComponents/Contact/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export  function SiteEmail() {
+  return (
+    <>
+    <p>info@unicodewebdesign.com</p>
+    <span class="icon-envelop text-red-500  mb-[4px]"></span>
+    </>      
+  )
+}
+
+export  function SitePhone() {
+    return (
+        <>
+        <p>09354502369</p>
+        <span class="icon-phone text-red-500 mb-[4px]"></span>
+        </>
+    )
+  }
+  
---
+++ src/Components/GeneralComponents/SocialIcons/SocialIcons.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SocialIcons() {
+  return (
+    <div className="flex gap-3 justify-center items-center">
+    <a href="#">
+      <span class="icon-github"></span>
+    </a>
+    <a href="#">
+    
+      <span class="icon-instagram"></span>
+    </a>
+    <a href="#">
+    
+      <span class="icon-telegram"></span>
+    </a>
+    <a href="#">
+    
+      <span class="icon-linkedin"></span>
+    </a>
+  </div>
+  )
+}
