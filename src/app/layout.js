2a9e9648bbabd22c846784a579ed98d8027c564b
oo11boy <editkhone@gmail.com>
2024-08-29 23:00:50 +0430

    feat:add swiper js and basic content slider

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fa">
       <body className={vazir.className}>{children}</body>
     </html>
   );
