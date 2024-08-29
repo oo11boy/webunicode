6f90811fbfd69dad83228882f2f22ffcad5b9fa5
oo11boy <editkhone@gmail.com>
2024-08-29 22:58:56 +0430

    feat:Expand the top header

---
+++ src/Components/GeneralComponents/SiteLogo/SiteLogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SmallLogo() {
+  return (
+    <img
+    className="skew-x-[15deg] h-[40px]"
+    src="https://validthemes.net/themeforest/wp/consua/wp-content/uploads/2023/08/logo.png"
+    alt=""
+    srcset=""
+  />
+  )
+}
