353314b26c32d3d2644ff445d91419d5fd1bcc93
oo11boy <editkhone@gmail.com>
2024-08-30 01:48:11 +0430

    feat:add animation to slider & Component separation

---
+++ src/Components/GeneralComponents/Sliders/IntroSlider/PrevNextButton/PrevNextButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PrevNextButton() {
+  return (
+    <>
+      {/* دکمه‌های ناوبری سفارشی */}
+      <button className="custom-next-button absolute bottom-5 left-10 z-10">
+        <span className="icon-arrow-right2"></span>
+      </button>
+      <button className="custom-prev-button absolute bottom-5 left-5 z-10">
+        <span className="icon-arrow-left2"></span>
+      </button>
+    </>
+  )
+}
