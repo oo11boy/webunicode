b27663253672e53a50e6958810aeaed3223bee23
oo11boy <editkhone@gmail.com>
2024-09-03 16:42:05 +0430

    feat: responsive portfolio slider

--- src/Components/GeneralComponents/Sliders/PortfolioSlider/PortfolioSlider.js
+++ src/Components/GeneralComponents/Sliders/PortfolioSlider/PortfolioSlider.js
@@ -1,29 +1,43 @@
 "use client"
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
-// import required modules
-import { Pagination } from 'swiper/modules';
 import SlideCard from './SlideCard/SlideCard';
 import './PortfolioSlider.css'
+
 export default function PortfolioSlider() {
   return (
     <div className='container-field py-44 bg-cover bg-[#F7F7F7] portfolio-slider-section'>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
-        pagination={{
-          clickable: true,
+        breakpoints={{
+          // when window width is >= 640px
+          290: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          770: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+      
+          // when window width is >= 768px
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          // when window width is >= 1024px
+          1500: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
         }}
-        modules={[Pagination]}
         className="Portfolio container"
       >
         <SwiperSlide><SlideCard/></SwiperSlide>
         <SwiperSlide><SlideCard/></SwiperSlide>
         <SwiperSlide><SlideCard/></SwiperSlide>
         <SwiperSlide><SlideCard/></SwiperSlide>
-      
       </Swiper>
     </div>
   );
--- src/Components/GeneralComponents/Sliders/PortfolioSlider/SlideCard/SlideCard.js
+++ src/Components/GeneralComponents/Sliders/PortfolioSlider/SlideCard/SlideCard.js
@@ -5,7 +5,7 @@ export default function SlideCard() {
     const [hoverlink, setHoverlink] = useState(false);
 
     return (
-        <div className="w-full slidecard relative bg-white rounded-xl overflow-hidden h-[540px]">
+        <div className="w-full slidecard  relative bg-white rounded-xl overflow-hidden h-[540px]">
             <img
                 className="h-[50%] w-full"
                 src="https://validthemes.net/themeforest/wp/consua/wp-content/uploads/2023/09/4.jpg"
@@ -15,13 +15,13 @@ export default function SlideCard() {
                 onMouseEnter={() => setHoverlink(true)}
                 onMouseLeave={() => setHoverlink(false)}
                 href="#"
-                className="w-[90%] flex bg-[#1D2027] h-20 absolute skew-x-12 right-[-5%] top-[42%]"
+                className="w-[90%] portfolio-slide-title flex bg-[#1D2027] h-20 absolute skew-x-12 right-[-5%] top-[42%]"
             >
-                <div className="skew-x-11 flex w-[40%] bg-red-700 justify-center items-center h-full">
+                <div className="skew-x-11 icontitletext flex w-[40%] bg-red-700 justify-center items-center h-full">
                     <span className="icon-office text-4xl -skew-x-12 text-white pr-4"></span>
                 </div>
                 <div className="flex justify-center items-center -skew-x-12 pr-4 text-xl">
-                    <div href="#" className="text-white">شرکتی مهندسی</div>
+                    <div href="#" className="text-white titletext">شرکتی مهندسی</div>
                 </div>
             </a>
             <p className="p-4 mt-16 h-[30%]">
