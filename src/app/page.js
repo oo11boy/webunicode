53e6af2f87686f42f885d73c2cd618a10476cf42
oo11boy <editkhone@gmail.com>
2024-09-07 21:18:10 +0430

    fix bug

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import FAQ from "@/Components/FAQ/FAQ";
 import Comments from "@/Components/Comments/Comments";
 import CategorySelection from "@/Components/CategorySelection/CategorySelection";
 import WhyUs from "@/Components/WhyUs/WhyUs";
-import ArticlesSection from "@/Components/IntroSlider/ArticlesSection/ArticlesSection";
+import ArticlesSection from "@/Components/ArticlesSection/ArticlesSection";
 export default function page() {
   return (
     <>
