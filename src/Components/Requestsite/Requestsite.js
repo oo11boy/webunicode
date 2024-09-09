1dd83a364e87feb9ab7ca0e987b70a236f9f1832
oo11boy <editkhone@gmail.com>
2024-09-09 22:37:49 +0430

    update

--- src/Components/Requestsite/Requestsite.js
+++ src/Components/Requestsite/Requestsite.js
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepButton from '@mui/material/StepButton';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
 
-const steps = ['اطلاعات خود را وارد کنید', 'انتخاب قالب', 'مشاهده قیمت'];
+const steps = ["اطلاعات خود را وارد کنید", "انتخاب قالب", "مشاهده قیمت"];
 
 export default function Requestsite() {
   const [activeStep, setActiveStep] = useState(0);
@@ -66,60 +66,61 @@ export default function Requestsite() {
   };
 
   return (
-    <Box sx={{ width: '100%' }} className='container mt-20'>
-      <Stepper nonLinear activeStep={activeStep}>
-        {steps.map((label, index) => (
-          <Step key={label} completed={completed[index]}>
-            <StepButton color="inherit" onClick={handleStep(index)}>
-              {label}
-            </StepButton>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {allStepsCompleted() ? (
-          <>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </>
-        ) : (
-          <>
-            <Typography component={"div"} sx={{ mt: 2, mb: 1, py: 1 }}>
-              {activeStep ===0 && <Step1/>}
-              {activeStep ===1 && <Step2/>}
-              {activeStep ===2 && <Step3/>}
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-              {activeStep !== steps.length &&
-                (completed[activeStep] ? (
-                  <Typography variant="caption" sx={{ display: 'inline-block' }}>
-                    Step {activeStep + 1} already completed
-                  </Typography>
-                ) : (
-                  <Button onClick={handleComplete}>
-                    {completedSteps() === totalSteps() - 1
-                      ? 'Finish'
-                      : 'Next'}
-                  </Button>
-                ))}
-            </Box>
-          </>
-        )}
-      </div>
-    </Box>
+    <div className="container-field bg-[#9844F1] h-lvh">
+      <Box sx={{ width: "100%" }} className="container pt-20">
+        <Stepper nonLinear activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label} completed={completed[index]}>
+              <StepButton color="inherit" onClick={handleStep(index)}>
+                {label}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {allStepsCompleted() ? (
+            <>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={handleReset}>Reset</Button>
+              </Box>
+            </>
+          ) : (
+            <>
+             <div className="h-[80vh] flex flex-col justify-between items-center w-[90%] m-auto border-2 border-gray-200 shadow-lg rounded-xl bg-white">
+                
+              <Typography className="w-full h-[90%]" component={"div"} sx={{ mt: 2, mb: 1, py: 1 }}>
+                 {activeStep === 0 && <Step1 />}
+                  {activeStep === 1 && <Step2 />}
+                  {activeStep === 2 && <Step3 />}
+             
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: "1 1 auto" }} />
+                {activeStep !== steps.length &&
+                  
+                    <Button onClick={handleComplete}>
+                     
+                        Next
+                    </Button>
+                }
+              </Box>
+              </div>
+            </>
+          )}
+        </div>
+      </Box>
+    </div>
   );
 }
--- src/Components/Requestsite/Step1.js
+++ src/Components/Requestsite/Step1.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Step1() {
   return (
-    <form action=""></form>
+    <div className='flex h-[100%] flex-col justify-center items-center'>
+    <h3 className='text-3xl mb-8'>اطلاعات زیر را وارد نمایید</h3>
+        <form action="" className='flex w-full flex-col justify-center items-center'>
+            <input type="text" name="" id="" className='bg-[#F7F8F9] border mb-4 border-gray-500 w-[60%] p-4 rounded-md' placeholder='نام و نام خانوادگی خود را وارد نمایید.'/>
+            <input type="number" className='bg-[#F7F8F9] border mb-4 border-gray-500 w-[60%] p-4 rounded-md' placeholder="شماره تماس خود را وارد نمایید."/>
+            <input type="mail" className='bg-[#F7F8F9] border mb-4 border-gray-500 w-[60%] p-4 rounded-md' placeholder='ایمیل خود را وارد کنید.'/>
+        </form>
+    </div>
   )
 }
