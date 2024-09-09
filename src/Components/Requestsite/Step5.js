64072469941afdfc335014eb3e2f9299d3975c12
oo11boy <editkhone@gmail.com>
2024-09-10 01:29:05 +0430

    feat: update step5 style

--- src/Components/Requestsite/Step5.js
+++ src/Components/Requestsite/Step5.js
@@ -7,16 +7,16 @@ export default function Step5({ step, setStep }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh]">
       <div className="p-6 rounded-lg text-center">
-        <CheckCircleIcon className="text-green-500" style={{ fontSize: 60 }} />
-        <h1 className="text-2xl font-bold mt-4 text-gray-800">درخواست شما ثبت شد</h1>
-        <p className="text-gray-600 mt-2">به زودی با شما تماس خواهیم گرفت</p>
-        <div className="text-gray-500 mt-4">شماره تماس ما: <SitePhone/></div>
+        <CheckCircleIcon className="text-[#1f9d7e]" style={{ fontSize: 60 }} />
+        <h1 className="text-2xl font-bold mt-4 text-[#9844F1]">درخواست شما ثبت شد</h1>
+        <p className="text-[#9844F1] mt-2">به زودی با شما تماس خواهیم گرفت</p>
+        <div className="text-[#1f9d7e] mt-4">شماره تماس ما: <SitePhone/></div>
         
         <Button 
           variant="contained" 
           color="primary" 
           onClick={() => window.location.href = '/'} 
-          className="mt-6"
+          className="mt-6 !bg-[#1f9d7e]" 
         >
           بازگشت به سایت
         </Button>
