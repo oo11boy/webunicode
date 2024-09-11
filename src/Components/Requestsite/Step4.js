7dd95100c9bf94cd7742a99534b445cb93155eae
oo11boy <editkhone@gmail.com>
2024-09-11 15:05:58 +0430

    next btn event

--- src/Components/Requestsite/Step4.js
+++ src/Components/Requestsite/Step4.js
@@ -150,7 +150,7 @@ export default function Step4({ step, setStep }) {
         </div>
 
         <div className="w-full lg:!w-1/3 h-full overflow-hidden bg-white rounded-xl shadow-lg border border-gray-200">
-          <button className="btnnext p-4 lg:!p-[unset] text-xl w-full h-full">
+          <button onClick={()=>setStep(5)} className="btnnext p-4 lg:!p-[unset] text-xl w-full h-full">
             ثبت درخواست مشاوره و تماس
           </button>
         </div>
