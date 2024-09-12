ebd6d4be33c4fa5330aa43cf4597b2b617bcbf80
Hesam <hrahmaniii20@gmail.com>
2024-09-12 11:18:36 +0330

    feat: add price format

--- src/Components/Requestsite/Step4/PriceCard.js
+++ src/Components/Requestsite/Step4/PriceCard.js
@@ -1,23 +1,30 @@
 import React from "react";
 
 export default function PriceCard({ checkout, infoorder, formData }) {
+  const priceFormat = (number) => {
+    return new Intl.NumberFormat().format(number);
+  };
+
+  console.log(checkout);
   return (
     <div className="w-full mb-4 lg:!mb-[unset] lg:!w-3/4 h-full flex justify-between items-center p-4 bg-white rounded-xl shadow-lg border border-gray-200">
       <h2 className="text-sm md:!text-lg">هزینه تقریبی برای این سفارش:</h2>
       <p className="text-sm md:!text-lg">
-      
-      {
-  (checkout
-    ? checkout.price
-    : infoorder.category == "store"
-    ? 6 * 1000000
-    : infoorder.category == "company"
-    ? 4 * 1000000
-    : 2 * 1000000) +
-  (formData.comdomain ? 1000000 : 0) +
-  (formData.extrahost == 5 ? 500000 : formData.extrahost == 10 ? 1000000 : 0)
-}
-
+        {priceFormat(
+          (checkout
+            ? checkout.price
+            : infoorder.category == "store"
+            ? 6 * 1000000
+            : infoorder.category == "company"
+            ? 4 * 1000000
+            : 2 * 1000000) +
+            (formData.comdomain ? 1000000 : 0) +
+            (formData.extrahost == 5
+              ? 500000
+              : formData.extrahost == 10
+              ? 1000000
+              : 0)
+        )}
         تومان
       </p>
     </div>
