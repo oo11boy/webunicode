e83cabd5aa37b62576470a604852b83c44bf777c
Hesam <hrahmaniii20@gmail.com>
2024-09-12 15:48:56 +0330

    feat: make site type component in step 1

--- src/Components/Requestsite/Step1/Step1.js
+++ src/Components/Requestsite/Step1/Step1.js
@@ -1,6 +1,7 @@
 import { PortfolioDb } from "@/lib/PortfolioDb";
 import { useEffect, useState } from "react";
 import { FaStore, FaBuilding, FaIndustry } from "react-icons/fa";
+import SiteType from "./SiteType";
 
 export default function Step2({ step, setStep, id }) {
   const [selectedType, setSelectedType] = useState("");
@@ -14,16 +15,17 @@ export default function Step2({ step, setStep, id }) {
   const handleSelectType = (value) => {
     setSelectedType(value);
     // Store the selected type in formData
-  
   };
 
   const isNextButtonEnabled = () => {
     return selectedType !== "";
   };
 
-  useEffect(()=>{
-    id>0 && setStep(2) 
-  },[id])
+  useEffect(() => {
+    id > 0 && setStep(2);
+    console.log('hello');
+    
+  }, [id]);
 
   return (
     <div className="flex step1 min-h-[80vh] step1 flex-col justify-center items-center">
@@ -31,28 +33,13 @@ export default function Step2({ step, setStep, id }) {
         نوع سایت را انتخاب نمایید:
       </h3>
       <div className="flex flex-wrap justify-center items-center gap-8">
-        {siteTypes.map((type) => (
-          <div
-            key={type.value}
-            onClick={() => handleSelectType(type.value)}
-            className={`relative p-8 w-32 h-32 lg:!w-64 lg:!h-64 cursor-pointer border rounded-3xl shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-blue-100 ${
-              selectedType === type.value
-                ? "border-blue-500 bg-blue-50"
-                : "bg-white"
-            }`}
-          >
-            <div className="flex flex-col items-center justify-center h-full">
-              <span className="text-3xl text-[#1f9d7e] sm:!text-5xl mb-4">
-                {type.icon}
-              </span>
-              <h4 className="text-xl text-[#077c5f] font-semibold">
-                {type.label}
-              </h4>
-            </div>
-            {selectedType === type.value && (
-              <div className="absolute top-0 left-0 right-0 bottom-0 rounded-3xl bg-blue-200 opacity-20"></div>
-            )}
-          </div>
+        {siteTypes.map((type, index) => (
+          <SiteType
+            type={type}
+            key={index}
+            selectedType={selectedType}
+            handleSelectType={handleSelectType}
+          />
         ))}
       </div>
 
