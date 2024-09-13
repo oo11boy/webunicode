7cfcc65839be1732d9b93f12d1d8b353b6363b54
oo11boy <editkhone@gmail.com>
2024-09-13 16:08:18 +0430

    update

--- src/app/[singlepage]/page.js
+++ src/app/[singlepage]/page.js
@@ -1,7 +1,6 @@
 import Footer from '@/Components/Footer/Footer'
 import TopHeader from '@/Components/Header/TopHeader'
 import WhyUs from '@/Components/SinglePageComponents/WhyUs/WhyUs'
-import DynamicComponent from '@/Components/UiComponents/DynamicComponent/DynamicComponent'
 
 import React from 'react'
 
@@ -9,8 +8,8 @@ export default function page({params}) {
   return (
     <div>
         <TopHeader/>
-      <DynamicComponent categoryname={params.singlepage}/>
-        <Footer/>
+    <WhyUs/>
+      <Footer/>
     </div>
   )
 }
