c09b723829ca63199ab91020c85a2c6eac3da865
oo11boy <editkhone@gmail.com>
2024-09-13 13:04:38 +0430

    basic single page

---
+++ src/app/[singlepage]/page.js
@@ -0,0 +1,13 @@
+import Footer from '@/Components/Footer/Footer'
+import TopHeader from '@/Components/Header/TopHeader'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <TopHeader/>
+
+        <Footer/>
+    </div>
+  )
+}
