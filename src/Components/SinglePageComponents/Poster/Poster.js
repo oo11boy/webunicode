6a802f8b621bb515c6aa6fc9c5833660e7d74e05
oo11boy <editkhone@gmail.com>
2024-09-14 23:05:16 +0430

    feat: add image in poster slider

--- src/Components/SinglePageComponents/Poster/Poster.js
+++ src/Components/SinglePageComponents/Poster/Poster.js
@@ -1,54 +1,74 @@
+import { useEffect, useState } from "react";
 import "./Poster.css";
 import RequestSiteBtn from "@/Components/UiComponents/Buttons/RequestSiteBtn";
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-
-// import required modules
-import { Autoplay, EffectCards } from 'swiper/modules';
-import { PortfolioDb } from "@/lib/PortfolioDb";
 
 export default function Poster() {
+  const [slider, setSlider] = useState(1);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFade(false);
+      setTimeout(() => {
+        setSlider((prev) => (prev === 5 ? 1 : prev + 1));
+        setFade(true);
+      }, 500);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [slider]);
+
+  const images = [
+    {
+      main: "https://i-wp.ir/wp-content/uploads/2022/11/custom1.jpg",
+      small: "/image/storeresponsive.png",
+    },
+    {
+      main: "https://i-wp.ir/wp-content/uploads/2024/08/aroj.jpg",
+      small: "/image/store2res.png",
+    },
+    {
+      main: "https://i-wp.ir/wp-content/uploads/2024/05/lighto.jpg",
+      small: "/image/storeres3.png",
+    },
+    {
+      main: "https://i-wp.ir/wp-content/uploads/2024/06/petland.jpg",
+      small: "/image/storeres4.png",
+    },
+    {
+      main: "https://i-wp.ir/wp-content/uploads/2022/11/cosmetic.jpg",
+      small: "/image/storeres5.png",
+    },
+  ];
+
   return (
     <div className="poster-bg container-field">
       <div className="container flex items-center justify-center lg:pt-[250px] pt-[100px]">
         <div className="flex lg:flex-row flex-col justify-between items-center w-full">
           <div className="lg:w-[40%] md:w-[60%] w-full space-y-5 pb-20 text-white">
             <h2 className="text-2xl text-start">قالب فروشگاهی</h2>
-            <h4 className="text-4xl font-bold text-start ">
+            <h4 className="text-4xl font-bold text-start">
               تیم قویه این طرحو با کمترین قیمت در اختیار شما قرار میده ددددد
             </h4>
-            <h4 className="text-xl font-bold text-start ">
+            <h4 className="text-xl font-bold text-start">
               سفارش بدید حالشو ببرید
             </h4>
-            <RequestSiteBtn text='درخواست سایت'/>
+            <RequestSiteBtn text="درخواست سایت" />
           </div>
-          <div className="relative lg:w-[50%] w-full rounded-lg flex justify-center">
-            <div className="z-20 w-full rounded-xl  absolute top-0">
-            <>
-      <Swiper
-        effect={'cards'}
-        autoplay={{ delay: 1000 }}  
-        loop={true}
-  grabCursor={true}
-  modules={[EffectCards, Autoplay]}
-         className="w-full"
-      >
-        {PortfolioDb.map((item)=>(
-  <SwiperSlide >
-  <img src={item.img} className="w-full m-auto !object-contain" alt="" srcset="" />
-</SwiperSlide>
-        ))}
-      
-       
-      </Swiper>
-    </>
- 
+          <div className="relative lg:w-[50%] w-full flex justify-center">
+            <div className={fade ? "show" : "fade"}>
+              <div className="z-20">
+                <img
+                  className="w-[95%] rounded-2xl lg:skew-x-6 lg:-skew-y-6"
+                  src={images[slider - 1].main}
+                  alt=""
+                />
+                <img
+                  className="w-[40%] rounded-2xl absolute bottom-0 lg:right-0 -right-5 lg:-skew-x-12 lg:skew-y-12"
+                  src={images[slider - 1].small}
+                  alt=""
+                />
+              </div>
             </div>
             <span className="poster-content"></span>
           </div>
