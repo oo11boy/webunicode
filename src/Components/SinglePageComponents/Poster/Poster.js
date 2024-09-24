e874b9de7423756294e65edafed1c6374b6a1eb9
oo11boy <editkhone@gmail.com>
2024-09-24 13:17:40 +0330

    feat:dynamic poster in single page

--- src/Components/SinglePageComponents/Poster/Poster.js
+++ src/Components/SinglePageComponents/Poster/Poster.js
@@ -9,9 +9,9 @@ export default function Poster({ singlepageid }) {
   const dynamicSinglePage = 
     singlepageid === 'store' ? [
       {
-        title: 'قالب فروشگاهی',
-        description: 'تیم قویه این طرحو با کمترین قیمت در اختیار شما قرار میده ددددد',
-        content: 'سفارش بدید حالشو ببرید',
+        title: 'خرید سایت فروشگاهی',
+        description: 'اگر به دنبال سایتی برای فروش محصولاتتان هستید یونیکد میتواند شما را در طراحی سایت ارزان فروشگاهی یاری نماید.',
+      
         images: [
           { main: "https://i-wp.ir/wp-content/uploads/2022/11/custom1.jpg", small: "/image/storeresponsive.png" },
           { main: "https://i-wp.ir/wp-content/uploads/2024/08/aroj.jpg", small: "/image/store2res.png" },
@@ -22,28 +22,22 @@ export default function Poster({ singlepageid }) {
       }
     ] : singlepageid === 'company' ? [
       {
-        title: 'قالب شرکتی',
-        description: 'تیم قویه این طرحو با کمترین قیمت در اختیار شما قرار میده ددددد',
-        content: 'سفارش بدید حالشو ببرید',
+        title: 'خرید سایت شرکتی',
+        description: 'اگر به دنبال سایتی برای معرفی کسب و کار و شرکت خودتان هستید یونیکد میتواند شما را در طراحی سایت شرکتی یاری نماید.',
+      
         images: [
-          { main: "https://i-wp.ir/wp-content/uploads/2022/11/custom1.jpg", small: "/image/storeresponsive.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/08/aroj.jpg", small: "/image/store2res.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/05/lighto.jpg", small: "/image/storeres3.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/06/petland.jpg", small: "/image/storeres4.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2022/11/cosmetic.jpg", small: "/image/storeres5.png" },
-        ],
+          { main: "https://wp-theme.xyz/images/1_1.jpg", small: "/image/c1.png" },
+          { main: "https://wp-theme.xyz/images/4_1.jpg", small: "/image/c2.png" },
+          ],
       }
     ] : singlepageid === 'resume' ? [
       {
-        title: 'قالب شخصی رزومه‌ایی',
-        description: 'تیم قویه این طرحو با کمترین قیمت در اختیار شما قرار میده ددددد',
-        content: 'سفارش بدید حالشو ببرید',
+        title: 'خرید سایت رزومه ای و شخصی',
+        description: 'اگر به دنبال سایتی برای معرفی خودتان هستید یونیکد میتواند شما را در طراحی سایت ارزان اقتصادی و رزومه ای یاری نماید.',
+      
         images: [
-          { main: "https://i-wp.ir/wp-content/uploads/2022/11/custom1.jpg", small: "/image/storeresponsive.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/08/aroj.jpg", small: "/image/store2res.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/05/lighto.jpg", small: "/image/storeres3.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2024/06/petland.jpg", small: "/image/storeres4.png" },
-          { main: "https://i-wp.ir/wp-content/uploads/2022/11/cosmetic.jpg", small: "/image/storeres5.png" },
+          { main: "/image/r1.png", small: "/image/rs1.png" },
+          { main: "/image/r2.png", small: "/image/rs2.png" },
         ],
       }
     ] : [];
@@ -68,22 +62,21 @@ export default function Poster({ singlepageid }) {
             <div key={index} className="flex lg:flex-row flex-col justify-between items-center w-full">
               <div className="lg:w-[40%] md:w-[60%] w-full space-y-5 pb-20 text-white">
                 <h2 className="text-2xl text-start">{page.title}</h2>
-                <h4 className="text-4xl font-bold text-start">{page.description}</h4>
-                <h4 className="text-xl font-bold text-start">{page.content}</h4>
-                <RequestSiteBtn text="درخواست سایت" />
+                <h4 className="text-2xl font-bold text-start">{page.description}</h4>
+                <RequestSiteBtn  text="درخواست سایت" />
               </div>
               <div className="relative z-50 lg:w-[50%] w-full flex justify-center">
                 <div className={fade ? "show" : "fade"}>
                   <div className="z-20">
                     <img
-                      className="w-[95%] rounded-2xl lg:skew-x-6 lg:-skew-y-6"
+                      className="w-[95%]  rounded-2xl lg:skew-x-6 lg:-skew-y-6"
                       src={page.images[slider - 1].main}
-                      alt=""
+                      alt={page.title}
                     />
                     <img
                       className="w-[40%] rounded-2xl absolute bottom-0 lg:right-0 -right-5 lg:-skew-x-12 lg:skew-y-12"
                       src={page.images[slider - 1].small}
-                      alt=""
+                      alt={page.title}
                     />
                   </div>
                 </div>
--- src/Components/UiComponents/Buttons/RequestSiteBtn.js
+++ src/Components/UiComponents/Buttons/RequestSiteBtn.js
@@ -4,7 +4,7 @@ import "./RequestSiteBtn.css";
 export default function RequestSiteBtn({text}) {
   return (
     <div class="button-container text-start flex ">
-      <a href="#">
+      <a href="../requestSite" target="_blank">
         <span>{text}</span>
       </a>
     </div>
