6b3c9a8d05b791a70011df3cfa1360a23b064774
oo11boy <editkhone@gmail.com>
2024-09-28 11:37:39 +0330

    feat: add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,51 @@
+import { getPosts } from "@/lib/DataFetching";
+
+
+
+
+export default async function sitemap() {
+  const dataposts = await getPosts();
+  const posts = dataposts.map((item) => ({
+    url: `https://unicodewebdesign.com/blogs/${item.id}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }));
+
+ 
+
+  return [
+    {
+      url: `https://unicodewebdesign.com`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `https://unicodewebdesign.com/store`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `https://unicodewebdesign.com/company`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `https://unicodewebdesign.com/goodprice`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+ 
+    {
+      url: `https://unicodewebdesign.com/Blogs`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    ...posts
+  ];
+}
