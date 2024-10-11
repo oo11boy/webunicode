41ac7591d8075e54a2b01b0e6bc8fa895568f129
oo11boy <editkhone@gmail.com>
2024-10-11 15:15:49 +0330

    update env

--- src/lib/DataFetching.js
+++ src/lib/DataFetching.js
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 
 export async function getPosts() {
-  let res = await fetch(`http://localhost:3000/api/posts`);
+  let res = await fetch(`${process.env.NEXT_PUBLIC_API}/posts`);
   let post = await res.json();
   if (!post) notFound();
   return post;
